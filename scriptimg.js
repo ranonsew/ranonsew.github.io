acdf85eedcd688ebe954181f81a0da33ce04d7d0
ranonsew <ranonsew@gmail.com>
2021-07-10 03:45:58 +0800

    olay
    ye

--- scriptimg.js
+++ scriptimg.js
@@ -8,3 +8,20 @@ const probably = (first, second) => {
 }
 
 console.log(probably(yes, perhaps()));
+
+myArr = [
+    {
+        name: "forsaken",
+        email: "forsaken@word.exe"
+    },
+    {
+        name: "bob",
+        email: "bob@showpls.net"
+    },
+    {
+        name: "polygone",
+        email: "polygone@wheresmypolygons.org"
+    }
+];
+
+console.log(myArr);
