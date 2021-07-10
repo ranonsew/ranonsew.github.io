74cde44676e120b01160d8b8fe5f074f806e0978
ranonsew <ranonsew@gmail.com>
2021-07-10 10:18:09 +0800

    script2 js added
    defer src added to project

---
+++ script2.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name,email){
+        this.name = name;
+        this.email = email;
+    }
+    getName(){
+        return this.name;
+    }
+}
+
+function yes(){
+
+}
