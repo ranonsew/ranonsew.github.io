b1ef7284f15e76ebfc4c1318ff132719336dfca7
ranonsew <ranonsew@gmail.com>
2021-07-11 16:08:31 +0800

    closures and functions practice
    more closure practice
    
    practicing functions
    differences between for in and for of, for in deals in the indexes, for of deals in the contents of what is looped
    
    using filter and find array methods
    
    closures and how to use them
    
    the var vs let closure question that is hard for some to understand, global vs block scope and also the setTimeout and how long it takes for 3 loops to occur
    
    a more complex closure example where an array of 2 functions is returned, and can be stored by 2 function names of our choosing by setting is like const [arr1, arr2] = returned_array().
    
    very interesting stuff

--- scriptimg.js
+++ scriptimg.js
@@ -99,16 +99,87 @@ emp_arr = [
 const search_name = (empArr, name) => {
     let emp = empArr.find(employee => employee.name === name);
     return emp.email;
-};
+}
 const filter_gender = (empArr, gender) => {
     let emp = empArr.filter(employee => employee.gender === gender);
     const emails = [];
-    for(e in emp){
+    for(e in emp){ // for indexes in the array, as apposed to other for loops
         console.log(e);
         emails.push(emp[e].email);
     }
+    // for(e of emp){
+    //     console.log(e);
+    //     // this is most like python's for in, since it deals not with 
+    //     // indexes, but objects themselves
+    //     emails.push(e.email);
+    // }
     return emails;
-};
+}
 
 console.log(search_name(emp_arr, "Fujiwara Takumi"));
 console.log(filter_gender(emp_arr, "F"));
+
+// perhaps some practice into closures is necessary too
+const closure_outside = (var_in) => {
+    let ok = var_in;
+    const closure_inside = () => {
+        console.log(ok);
+    }
+    return closure_inside();
+}
+closure_outside("crap");
+
+const closure_out_2 = (number) => {
+    const x_arr = [number, number*2, number*3];
+    const closure_in_2 = () => {
+        for(x of x_arr){
+            console.log(x);
+        }
+    }
+    return closure_in_2();
+}
+
+closure_out_2(8);
+
+//----------------------------------------------------------------------//
+// closure question that might be difficult for some to understand
+for(var i=0; i<3; i++){
+    const log = () => {console.log(i);}
+    setTimeout(log, 100);
+}
+// vs
+for(let j=0; j<3; j++){
+    const logging = () => {console.log(j)};
+    setTimeout(logging, 100);
+}
+//why? Because var is global scope, let is block scope.
+// i.e. setTimeout saves the reference to var (not the data of it)
+// whereas setTimeout saves the data in let (not (just) the reference)
+// also the loop happens faster than the setTimeout of 100ms, so 3 setTimeouts are
+// stored in the call-stack. 
+// so with var, 3 var references are stored in the stack, at the end of the 3 loops,
+// var is 3, and therefore the timeouts print 3,3,3
+// but with let, data from each let is stored in the stack, so at the end of 3 loops,
+// 0,1,2 are stored in the stack, so the timeouts print 0,1,2
+//----------------------------------------------------------------------//
+
+
+// something from fireship that I should understand to improve my understanding
+// of functions and closures
+function useCat(){
+    let name = "baby kitten";
+    return [
+        () => `Meow ${name}`, // no need for a return statement as js automatically assumes it with this type of statement
+        (newName) => name = newName
+    ]; // returns an array with 2 functions
+}
+const [meow, setName] = useCat(); // creating a const array, with meow as the first
+                                    // returned array function, and setname as
+                                    // the second returned array function
+// like so, see meow is now the first returned function
+console.log(meow());
+// and the second one, sets the current name in the outer closure lexical environment
+// to the newName, so when meow() is called, this is returned
+setName("tommy");
+console.log(meow());
+
