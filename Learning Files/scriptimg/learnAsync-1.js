b146a95bd09953458701d5795bee5cf8b77b4ed3
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2021-08-28 16:43:03 +0800

    async await learning
    async await learning
    js promises chaining
    2nd async file
    tester html for using browser apis

--- Learning Files/scriptimg/learnAsync-1.js
+++ Learning Files/scriptimg/learnAsync-1.js
@@ -28,4 +28,60 @@ console.log("Synchronous 4");
 
 
 // so onto some more async await stuff and promises
+// async and await are essentially syntactical sugar to make promises easier
+// i.e. understanding promises is essential to understand async and await
+// so first, some functions that return promises (web dev simplified) to help us understand with and without async await
+function makeRequest(location) { // takes location, and resolves only if "Google" is location
+    return new Promise((resolve, reject) => {
+        console.log(`Making request to ${location}`);
+        if (location === "Google"){ // must be "Google", cannot be "google", etc
+            resolve("Google says hi");
+        } else {
+            reject("We can only talk to google");
+        }
+    });
+}
+function processRequest(response) { // this one only resolves, and adds extra info
+    return new Promise((resolve, reject) => {
+        console.log("Processing response");
+        resolve(`Extra information + ${response}`);
+    });
+}
 
+// so as always, first using the 2 functions without async await (and only promises)
+        // makeRequest("Google").then(response => {
+        //     console.log("Response received");
+        //     return processRequest(response);
+        // }).then(processedResponse => {
+        //     console.log(processedResponse);
+        // }).catch(error => {
+        //     console.log(error);
+        // });
+// if "Google", then 4 console logs, otherwise, 2 console logs, You'll see em
+// not too bad, but can be easier to understand without all the .then
+// but if we need to use the same parameter inside the first and second .then
+// need to create a variable called like something="item"
+
+// okay so how do we make this simpler?
+// async await!
+async function doWork() { // need to tell js this func is async, so now this func returns a promise
+    try {
+        let response = await makeRequest("Google"); 
+        // await -- code should wait until the makeRequest it finished
+        // because this is in async function, js will leave this func to do other work in program, 
+        // so when makeRequest is finished executing, 
+        // it will return the resolve of makeRequest into the response variable
+        // so the stuff in this async function will only continue to run after awaited stuff has resolved or rejected 
+        console.log("Response received");
+        let processedResponse = await processRequest(response); // same thing here
+        console.log(processedResponse);
+    } catch(error) {
+        // so for catching errors, we use a try catch block instead
+        // resolve stuff goes into try, and if there's an error thrown at any point,
+        // catch will immediately return the stuff inside here.
+        console.log(error);
+    }
+}
+doWork(); // in terms of code length, async await is shorter and easier to implement
+// no need for callback pyramid of doom, or callback hell, also no need for then list of doom
+// but in the future will most likely have a lot of try catch statements instead lmao
---
+++ Learning Files/scriptimg/learnAsync-2.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// a lot of comment info in the first one, and I assume the same for this one
+// based on fireship's "the async await episode I promised"
