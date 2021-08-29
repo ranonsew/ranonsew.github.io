f0411a5e8bd40f3e3ce4197684ed22ee5045e260
ranonsew <ranonsew.2020@scis.smu.edu.sg>
2021-08-29 19:56:27 +0800

    comment edit
    edited comments to be more concise for re-reading and stuff

--- Learning Files/scriptimg/learnAsync/learnAsync-1.js
+++ Learning Files/scriptimg/learnAsync/learnAsync-1.js
@@ -1,24 +1,23 @@
 "use strict";
 
-// WIP, learning concept still before examples are added for practice and re-reading
 
-
-// Async Await -- no need for callbacks
+// Async Await on promises -- no need for callbacks, easier js promises
 // asynchronous functionality that do not slow down the process cycle
-// which is a nice touch when certain items do not require a process before it
-// to finish in order to complete its own things (and things which do await for it)
-// so an async function will say that they will do their own things in the process cycle
-// then await stuff will be the things awaiting the async to finish so they can be 
-// added to the callstack
+// nice touch when certain items do not require a process before it to finish 
+// in order to complete its own things (e.g. b does not need a in order to run, etc)
+// async functions return value as promise resolve, and indicate that if there is
+// await keyword, that function will continue on a micro task loop so the rest of the code
+// can run, before it is done and then called back into the main event loop callstack
 
-// there are things that are called micro and macro tasks
-// in the (single threaded) event loop, when an asynchronous task is called,
-// it tells the event loop, like here's a function I need to run, but I need to get some
-// data from a db first. So the event loop says okay, and continues doing other things while the data is being called.
-// that is done on a separate event loop, and when that is finished, it will let the event loop know
-// that it is ready to be called back, but here's the micro macro task thing
-// if it's a setTimeout or setInterval (macro tasks), it will be called back at the start of the next event loop
-// else it is a micro task and it is called before the start of the next event loop
+// there are micro and macro tasks
+// in (single threaded) event loop, when an async task is called, it tells event loop 
+// hey, here's a function I need to run, but I need to get some data from a db first. 
+// So event loop says okay, and continues doing other things while the data is being called.
+// It's done on a separate event loop, and when it's finished, it will let the event loop know
+// that it's ready to be called back. 
+// Okay so here's the part about micro/macro tasks:
+    // if it's setTimeout/setInterval (macro), called back *at* the start of the next event loop
+    // else (micro task) and called *before* the start of the next event loop
 // thx fireship.io, here's their example
 console.log("Synchronous 1");
 setTimeout(_ => console.log("Timeout 2"), 0); // queued as a macro task
@@ -27,9 +26,8 @@ console.log("Synchronous 4");
 // Synch 1, Synch 4, Promise 3, Timeout 2
 
 
-// so onto some more async await stuff and promises
-// async and await are essentially syntactical sugar to make promises easier
-// i.e. understanding promises is essential to understand async and await
+// async await is essentially syntactical sugar to make promises easier
+// i.e. understanding promises is essential to understand async await
 // so first, some functions that return promises (web dev simplified) to help us understand with and without async await
 function makeRequest(location) { // takes location, and resolves only if "Google" is location
     return new Promise((resolve, reject) => {
@@ -48,7 +46,7 @@ function processRequest(response) { // this one only resolves, and adds extra in
     });
 }
 
-// so as always, first using the 2 functions without async await (and only promises)
+// so first without async await (only promises)
         // makeRequest("Google").then(response => {
         //     console.log("Response received");
         //     return processRequest(response);
@@ -58,30 +56,27 @@ function processRequest(response) { // this one only resolves, and adds extra in
         //     console.log(error);
         // });
 // if "Google", then 4 console logs, otherwise, 2 console logs, You'll see em
-// not too bad, but can be easier to understand without all the .then
-// but if we need to use the same parameter inside the first and second .then
-// need to create a variable called like something="item"
+// not too bad, but can be easier to understand without all the .then().
+// e.g. if we need to use the same parameter inside the first and second .then,
+// we would need to create a variable called like something="item", then put it into .then()
 
 // okay so how do we make this simpler?
-// async await!
-async function doWork() { // need to tell js this func is async, so now this func returns a promise
+async function doWork() { // need to tell js this func is async, so now it returns a promise
     try {
         let response = await makeRequest("Google"); 
-        // await -- code should wait until the makeRequest it finished
-        // because this is in async function, js will leave this func to do other work in program, 
-        // so when makeRequest is finished executing, 
-        // it will return the resolve of makeRequest into the response variable
-        // so the stuff in this async function will only continue to run after awaited stuff has resolved or rejected 
+        // await -- code in this function must wait until the makeRequest resolves
+        // since this is in async, js leaves this func to do other work in program, 
+        // when makeRequest returns a promise of the value into variable response, function will continue to run.
+        // stuff in async function only continues to run after awaited stuff has resolved/rejected 
         console.log("Response received");
         let processedResponse = await processRequest(response); // same thing here
         console.log(processedResponse);
     } catch(error) {
         // so for catching errors, we use a try catch block instead
-        // resolve stuff goes into try, and if there's an error thrown at any point,
-        // catch will immediately return the stuff inside here.
+        // if there is an error/reject at any point, we will catch it here
         console.log(error);
     }
 }
 doWork(); // in terms of code length, async await is shorter and easier to implement
-// no need for callback pyramid of doom, or callback hell, also no need for then list of doom
-// but in the future will most likely have a lot of try catch statements instead lmao
+// no need for callback hell, also no need .then() chain of doom
+// in the future will most likely have a lot of try catch statements instead lmao
--- Learning Files/scriptimg/learnAsync/learnAsync-2.js
+++ Learning Files/scriptimg/learnAsync/learnAsync-2.js
@@ -1,51 +1,54 @@
 "use strict";
 
-// a lot of comment info in the first one, and I assume the same for this one
-// based on fireship's "the async await episode I promised"
 
-// first let's create a tick counting function (the one used by fireship)
+// tick counting function (used by fireship)
 let ticks = Date.now();
 const log = (v) => { // annonymous arrow function expression
     console.log(`${v} \n Elapsed: ${Date.now() - ticks}ms`);
-    // while loop -- sync 1: ~0ms; Billion complete: ~4000ms; sync 2: ~40001ms
-    // Promises (mistake) -- sync 1: ~0ms; sync 2: ~4000ms; Billion complete: ~40001ms
-    // promises.resolve().then() (async) -- sync 1: ~0ms; sync 2: ~1ms; Billion complete: ~4000ms
+    // comparisons of what is delay when using various methods in codeBlock()
+        // while loop -- s1: ~0ms; Billion: ~4000ms; s2: ~40001ms
+        // Promises (mistake) -- sync1: ~0ms; s2: ~4000ms; Billion: ~40001ms
+        // promises.resolve().then() (async) -- s1: ~0ms; s2: ~1ms; Billion: ~4001ms
+    // we can see how the below code is async or not with these results
 } 
 const codeBlock = () => {
         // Blocking code
-        // bad because makes function return synchronously, so outside stuff that doesn't
-        // rely on this function can't work until billion while loops done
+        // bad: makes function return sync, outside stuff that doesn't rely 
+        // on this function can't work until billion loops done
     // let i = 0;
     // while(i < 1000000000) { i++; }
     // return "Billion loops complete";
 
+
         // Async blocking code
-        // bad because the promise and the while loop still on the main event thread
-        // only the resolving of the value happening on the micro task thread
-        // sync 2 still has 4000ms because while loop is blocking
-        // but it returns before the resolve because the resolve goes onto the micro task 
-        // (async await solves this issue I think)
+        // bad: promise and while loop still on main event thread
+        // only the resolving of the value happens on the micro task thread
+        // so sync 2 still has 4000ms because the while loop is blocking it
+        // and it can only return before the resolve 
+        // because the resolve goes onto the micro task thread
     // return new Promise((resolve, reject) => {
     //     let i = 0;
     //     while(i < 1000000000) { i++; }
     //     resolve("Billion loops complete");
     // });
 
-        // Non-blocking async code, we can use this instead of the above
-        // good, because we are using the resolve (which goes into micro task thread)
-        // to return the while loop, so since the resolving goes into the micro task thread,
-        // so does the while loop that is in the resolving .then()
+
+        // Non-blocking async code, this solves the above issues
+        // good, because we are using Promise.resolve 
+        // (since as we see above, the resolve goes into the micro task thread)
+        // since billion loops only run after the Promise.resolve()
+        // the loops are run in the micro task thread, allowing sync2 to run first
     return Promise.resolve().then(v => {
         let i = 0;
-        while(i < 1000000000) { i++; }
-        return "Billion loops complete";
-    });
+        while(i < 1000000000) { i++; } // so this runs on the micro task thread
+        return "Billion loops complete"; // does not affect sync2 which comes after
+    }); // this is the power of asynchronous js
 }
 
 // Ideal situation: synchro 1, synchro 2, then delay before codeBlock finally returns
 // less than ideal: delay between synchro 1 & 2, because codeBlock didn't async
 log("Synchronous 1");
-// log(codeBlock()); // use this for non-promises
+// log(codeBlock()); // used for non-promises
 codeBlock().then(log); // use this for promises
 log("Synchronous 2");
 
@@ -56,12 +59,11 @@ const codingBlocker_2 = async () => {
     while(i < 1000000000) { i++; }
 }
 // not sure if this works actually, so I think I need to continue learning to find out
-// but I'll leave this here for now
+// but I'll leave this here for now (because I absolutely suck ass)
 
 
 
-
-// so promises are asynchronous in nature, and resolve is definitely going to be in the micro task thread type thing
-// promises are also difficult to follow if a lot of async events are chained up
-// async await is supposed to make promises easier to allow it to be readable like synchronous code
-// continued in learnAsync-3.js
+// so promises are asynchronous in nature,
+// and resolve is definitely going to be in the micro task thread
+// promises are difficult to follow if there are a lot of .then() callbacks
+// async await is supposed to make promises more readable (and also asynchronous?)
